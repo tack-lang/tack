6d836cdba917f2ba07208886a5f0cc1b2269744e
Akira <akiramusic000@gmail.com>
2025-09-14 17:32:18 -0500

    Fix CI?

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
                 .product(name: "Rainbow", package: "Rainbow")
             ]
         ),
-        .testTarget(name: "TackTest", dependencies: [.target(name: "Tack")])
+        .testTarget(name: "TackTest", dependencies: ["Tack"], path: "Tests/TackTest")
     ]
 )
